2956fd7d7730a4f6f232f78e293f9dded4ec688c
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-10 15:01:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,9 +19,9 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-
+hash.new
   {:key => value}
-  hash.new
+
   # return a hash that includes the key and value parameters passed into this method
 end
 
